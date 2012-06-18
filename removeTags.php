9651051fe6ace2c9c2b708f7071c9d1e27bc098a
Tubebaum <tubebaum@gmail.com>
2012-06-18 10:10:39 -0400

    Updated the loop

--- removeTags.php
+++ removeTags.php
@@ -11,55 +11,10 @@ $client->setKs($ks);
 $tags = $_REQUEST['tags'];
 if($_REQUEST['tags'] != "null") {
 	//Removes the tags from the cache
-	foreach($tags as $deleteTag) {
-		if(array_key_exists($deleteTag, $tagArray)) {
-			unset($tagArray[$deleteTag]);
-		}
-	}
+	foreach($tags as $deleteTag)
+		unset($tagArray[$deleteTag]);
+
 	$tagString = implode(',', $tags);
-	//Creates a filter to only discover entries with the necessary tags
-	$filter = new KalturaMediaEntryFilter();
-	$filter->orderBy = KalturaPlayableEntryOrderBy::CREATED_AT_DESC;
-	$filter->tagsMultiLikeOr = $tagString;
-	$pager = new KalturaFilterPager();
-	$pageSize = 500;
-	$pager->pageSize = $pageSize;
-	$pager->pageIndex = 1;
-	$results = $client->media->listAction($filter, $pager);
-	if(count($results->objects) > 0) {
-		$count = $results->totalCount;
-		$endPage = (int)($count / $pageSize);
-		$endPage += ($count % $pageSize == 0) ? 0 : 1;
-		//Iterates through all the pages of entries that match the filter
-		for($pageNum = 1; $pageNum <= $endPage; ++$pageNum) {
-				$pager->pageIndex = $pageNum;
-				if($pageNum > 1)
-					$results = $client->media->listAction($filter, $pager);
-			foreach($results->objects as $place => $entry) {
-				//Creates a temporary array for the entry's tags
-				$oldTags = explode(',', $entry->tags);
-				foreach($oldTags as $index => $tag) {
-					$oldTags[$index] = trim($tag);
-				}
-				//Removes the appropriate tags from the array
-				foreach($tags as $tag) {
-					if(in_array($tag, $oldTags))
-						unset($oldTags[array_search($tag, $oldTags)]);
-				}
-				//Updates the corresponding media entry with the new list of tags
-				$joinedTags = implode(', ', $oldTags);
-				$mediaEntry = new KalturaMediaEntry();
-				$mediaEntry->tags = $joinedTags;
-				$entryId = $results->objects[$place]->id;
-				$updateResults = $client->media->update($entryId, $mediaEntry);
-			}
-		}
-	}
-	file_put_contents(TAG_CACHE, serialize($tagArray));
-	print "Tags removed";
-}
-elseif(array_key_exists('null', $tagArray)) {
-	unset($tagArray['null']);
 	$pager = new KalturaFilterPager();
 	$pageSize = 500;
 	$pager->pageSize = $pageSize;
@@ -78,7 +33,53 @@ elseif(array_key_exists('null', $tagArray)) {
 		if(count($results->objects) == 0) {
 			$cont = false;
 		}
-		$entryIds = "";
+		foreach($results->objects as $place => $entry) {
+			$oldTags = explode(',', $entry->tags);
+			foreach($oldTags as $index => $tag) {
+				$oldTags[$index] = trim($tag);
+			}
+			foreach($tags as $tag) {
+					if(in_array($tag, $oldTags))
+						unset($oldTags[array_search($tag, $oldTags)]);
+			}
+			//Updates the corresponding media entry with the new list of tags
+			$joinedTags = implode(', ', $oldTags);
+			$mediaEntry = new KalturaMediaEntry();
+			$mediaEntry->tags = $joinedTags;
+			$entryId = $results->objects[$place]->id;
+			$updateResults = $client->media->update($entryId, $mediaEntry);
+				
+			if($lastCreatedAt != $entry->createdAt)
+				$lastEntryIds = "";
+			if($lastEntryIds != "")
+				$lastEntryIds .= ",";
+			$lastEntryIds .= $entry->id;
+			$lastCreatedAt = $entry->createdAt;
+		}
+	}
+	file_put_contents(TAG_CACHE, serialize($tagArray));
+	print "Tags removed";
+}
+elseif(array_key_exists('null', $tagArray)) {
+	unset($tagArray['null']);
+	$pager = new KalturaFilterPager();
+	$pageSize = 500;
+	$pager->pageSize = $pageSize;
+	$lastCreatedAt = 0;
+	$lastEntryIds = "";
+	$cont = true;
+	while($cont) {
+		$filter = new KalturaMediaEntryFilter();
+		$filter->orderBy = "-createdAt";
+		$filter->tagsMultiLikeOr = 'null';
+		if($lastCreatedAt != 0)
+			$filter->createdAtLessThanOrEqual = $lastCreatedAt;
+		if($lastEntryIds != "")
+			$filter->idNotIn = $lastEntryIds;
+		$results = $client->media->listAction($filter, $pager);
+		if(count($results->objects) == 0) {
+			$cont = false;
+		}
 		foreach($results->objects as $place => $entry) {
 			$oldTags = explode(',', $entry->tags);
 			foreach($oldTags as $index => $tag) {

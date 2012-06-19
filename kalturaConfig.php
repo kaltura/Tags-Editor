a569d8fffe3bafb3bbf51be290f9bb6d2ae27e44
Tubebaum <tubebaum@gmail.com>
2012-06-19 11:33:14 -0400

    Added the config file

---
+++ kalturaConfig.php
@@ -0,0 +1,5 @@
+<?php
+define("PARTNER_ID",xxxx); //Replace xxxx with your partner id
+define("ADMIN_SECRET",'xxxx'); //Replace xxxx with your admin secret
+define("USER_ID",'tagsManager');
+define("TAG_CACHE", 'tagCaching.txt');

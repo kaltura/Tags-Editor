8755a40db86a54e32d1ea6fb7253fd4cd2038dfa
Tubebaum <tubebaum@gmail.com>
2012-06-19 16:02:47 -0400

    Changed config directions

--- kalturaConfig.php
+++ kalturaConfig.php
@@ -3,8 +3,8 @@
 // Get a free trial at: http://corp.kaltura.com 
 // In your Kaltura account, get the partner Id and API Admin Secret from:
 // http://www.kaltura.com/index.php/kmc/kmc4#account|integration
-define("PARTNER_ID",pppp); //Replace pppp with partner ID
-define("ADMIN_SECRET",'ssss'); //Replace ssss with admin secret
+define("PARTNER_ID",xxxx); //Replace xxxx with partner ID
+define("ADMIN_SECRET",'xxxx'); //Replace xxxx with admin secret
 define("USER_ID",'tagsManager');
 define("PLAYER_UICONF_ID", 4289612);
 define("TAG_CACHE", 'tagCaching.txt');

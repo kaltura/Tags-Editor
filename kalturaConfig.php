fc7ddfc8909aa048438bb83fe463bace5b01db05
Tubebaum <tubebaum@gmail.com>
2012-07-09 17:07:54 -0400

    Minor edit

--- kalturaConfig.php
+++ kalturaConfig.php
@@ -10,4 +10,4 @@ define("PLAYER_UICONF_ID", 4289612);
 define("TAG_CACHE", 'tagCaching.txt');
 define("PHP_AIKSAURUS", '"http://204.236.255.97/phpa.php"'); //This is the default script (PHP AIKSAURUS) for finding synonyms hosted on the Kaltura server.
 define("BIG_HUGE_THESAURUS_KEY", 'xxxx'); //Replace xxxx with your Big Huge Thesaurus API Key. You may obtain an API key at http://words.bighugelabs.com/api.php
-define("USE_BIG_HUGE_THESAURUS", 'true'); // Defaults to use PHP Aiksaurus, set this to true and set your Big Huge Thesaurus API Key to use that library instead.
+define("USE_BIG_HUGE_THESAURUS", 'false'); // Defaults to use PHP Aiksaurus, set this to true and set your Big Huge Thesaurus API Key to use that library instead.
